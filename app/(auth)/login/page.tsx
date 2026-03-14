3bf6aa00ed9082dbcb0b683abd6df6a34a4e4ad0
Geoffrey-Owuor <geoffreyowuor22@gmail.com>
2026-03-14 10:42:09 +0300

    page metadata for auth pages

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,4 +1,10 @@
 import Login from "@/components/AuthPages/Login";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login",
+  description: "IssueDesk login page",
+};
 
 const page = () => {
   return <Login />;
