8b77f0f5c971c931327004d0116d079d901a578e
Geoffrey-Owuor <geoffreyowuor22@gmail.com>
2026-08-19 09:16:11 +0300

    Fixed overlfow-x issue in ViewArticle page

--- components/Modules/ArticlesPage/ViewArticle.tsx
+++ components/Modules/ArticlesPage/ViewArticle.tsx
@@ -82,7 +82,7 @@ const ViewArticle = ({ uuid }: ViewArticleProps) => {
     <div
       className={`mb-8 flex flex-col px-4 py-6 md:py-3.5 ${paddingX} lg:flex-row lg:gap-6`}
     >
-      <article className="w-full max-w-none">
+      <article className="w-full min-w-0 max-w-none">
         {/* Header Section */}
         <header className="mb-6">
           {/* Title */}
@@ -145,7 +145,7 @@ const ViewArticle = ({ uuid }: ViewArticleProps) => {
         <div className="mb-8 h-px bg-linear-to-r from-transparent via-neutral-300 to-transparent sm:mb-12 dark:via-neutral-700" />
 
         {/* Content Section */}
-        <div className="prose prose-lg dark:prose-invert prose-img:rounded-xl prose-headings:font-semibold prose-a:text-neutral-700 dark:prose-a:text-neutral-300 prose-code:before:content-none prose-code:after:content-none prose-code:rounded-md prose-code:bg-neutral-100 dark:prose-code:bg-neutral-800 prose-code:px-1.5 prose-code:py-0.5 prose-code:font-normal prose-code:text-[0.875em] max-w-none wrap-break-word">
+        <div className="prose prose-lg dark:prose-invert prose-img:max-w-full prose-img:rounded-xl prose-headings:font-semibold prose-a:text-neutral-700 dark:prose-a:text-neutral-300 prose-code:before:content-none prose-code:after:content-none prose-code:rounded-md prose-code:bg-neutral-100 dark:prose-code:bg-neutral-800 prose-code:px-1.5 prose-code:py-0.5 prose-code:font-normal prose-code:text-[0.875em] max-w-none wrap-break-word">
           <ReactMarkdown
             remarkPlugins={[remarkGfm]}
             rehypePlugins={[rehypeHighlight]}
